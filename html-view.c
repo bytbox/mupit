66edc7e180db9ee444561503559f422b8543e055
Scott Lawrence <srl@umd.edu>
2011-10-09 19:13:44 -0400

    Succesfully displaying LICENSE text as HTML

--- html-view.c
+++ html-view.c
@@ -2,9 +2,18 @@
 
 #include <gtkhtml/gtkhtml.h>
 
+#include <string.h>
+
 #include "common.h"
 
+GtkHTML *gtk_html = NULL;
+
 void prepare_html_view() {
-	view_widget = gtk_html_new();
+	if (!gtk_html) gtk_html = GTK_HTML(gtk_html_new());
+	view_widget = GTK_WIDGET(gtk_html);
+}
+
+void update_html_view() {
+	gtk_html_load_from_string(gtk_html, result_content, strlen(result_content));
 }
 
--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 #include "common.h"
 
 void markdown_make(char *filename);
+void prepare_html_view();
+void update_html_view();
 
 extern char ui_glade[];
 extern int ui_glade_len;
 
 GtkBuilder *builder;
 GtkAboutDialog *ad;
+GtkTextView *textview;
+GtkViewport *view_viewport;
 
 char *result_content;
 enum result_type_e result_type;
@@ -36,13 +40,21 @@ int main (int argc, char *argv[]) {
 
 	window = GTK_WIDGET (gtk_builder_get_object (builder, "window1"));
 	ad = GTK_ABOUT_DIALOG (gtk_builder_get_object(builder, "about"));
+	textview = GTK_TEXT_VIEW (gtk_builder_get_object(builder, "textview"));
+	view_viewport = GTK_VIEWPORT (gtk_builder_get_object(builder, "view_viewport"));
+
 	gtk_builder_connect_signals (builder, NULL);
 	g_object_unref (G_OBJECT (builder));
 	
 	g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 	g_signal_connect(window, "key-press-event", G_CALLBACK (key_press_event), NULL);
 
-	gtk_widget_show(window);
+	markdown_make("LICENSE");
+	prepare_html_view();
+	update_html_view();
+	gtk_container_add(GTK_CONTAINER(view_viewport), view_widget);
+
+	gtk_widget_show_all(window);
 	gtk_main();
 
 	return 0;
