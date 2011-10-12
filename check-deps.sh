d68eb1cd4bb5f42c3b11ac39237f63c9fe4b20d7
Scott Lawrence <bytbox@gmail.com>
2011-10-12 14:20:32 -0400

    Cleanup up check-deps a bit

--- check-deps.sh
+++ check-deps.sh
@@ -4,8 +4,7 @@
 
 PKGS="gtk+-3.0 evince-document-3.0 evince-view-3.0 libgtkhtml-4.0"
 for p in $PKGS; do
-	pkg-config --exists $p
-	if [ $? -gt 0 ]; then
+	if ! pkg-config --exists $p; then
 		echo "package $p not found"
 		exit 1
 	fi
